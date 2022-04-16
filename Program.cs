97f5be18a63ad4594260dfc8324d3f1615dc1d33
guyma <guyma@GUY-4600H-WIN11>
2022-04-16 17:39:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+	public static long Product(long input)
+    {
+		long result = 1;
+
+		//get each digit by mod instead of string conversion
+		while (input > 0)
+        {
+			result *= input % 10;
+			input /= 10;
+        }
+
+		return result;
+    }
+
+	public static long MultiplicativePersistence(long userInput)
+    {
+		long steps = 0;
+
+		//10 is smallest double digit number
+		while (userInput >= 10)
+        {
+			userInput = Product(userInput);
+			steps++;
+        }
+
+		return steps;
+    }
+    public static void Main(string[] args)
+    {
+		//largest step count discovered for number 277777788888899
+
+		long highestStepsCount = 0;
+		long highestStepsNumber = 0;
+
+		long start = 77551000000;
+		long finish = 1000000000000000;
+
+        for (long number = start; number < finish; number++)
+        {
+			//Console.WriteLine($"{number}: {MultiplicativePersistence(number)}");
+			if (MultiplicativePersistence(number) > highestStepsCount)
+            {
+				highestStepsCount = MultiplicativePersistence(number);
+				highestStepsNumber = number;
+            }
+			if (number % 1000000 == 0)
+            {
+				Console.WriteLine($"Upto {number} so far: {highestStepsNumber}");
+            }
+        }
+
+		Console.WriteLine($"Highest step count: {highestStepsNumber} at {highestStepsCount}");
+	}
+}
